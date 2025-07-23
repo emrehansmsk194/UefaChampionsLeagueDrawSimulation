1206748701a56d53c2578689f758da9bc9e1930c
Bekir Emrehan Şimşek <emrehansimsek194@gmail.com>
2025-07-24 02:18:34 +0300

    api url changed.

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create( {
-    baseURL: 'https://localhost:7285/api',
+    baseURL: 'https://uefachampionsleaguedrawsimulation-production-319a.up.railway.app/api',
     headers: {
         'Content-Type': 'application/json',
         
